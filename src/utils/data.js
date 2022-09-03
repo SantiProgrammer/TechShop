080d32f985e92e8e449b1e4132e4740fd6b794e3
santi-iztli <santi.iztli@gmail.com>
2022-09-03 13:32:07 -0500

    mac add

--- src/utils/data.js
+++ src/utils/data.js
@@ -4,7 +4,7 @@ export const films = [
     name: "Laptop Acer",
     price: 8000,
     stock: 450,
-    image: "https://i.ibb.co/hLxLvWV/acer.webp",
+    image: "https://i.ibb.co/h8y7Rhg/acer.webp",
     description: "Almacenamiento de alta velocidad, Con una SSD PCIe M.2 los usuarios tienen una gran velocidad para todos sus archivos multimedia y de trabajo, entre otros ",
     min: 1,
     category: 'windows'
@@ -14,7 +14,7 @@ export const films = [
     name: "Laptop Asus",
     price: 8500,
     stock: 400,
-    image: "https://i.ibb.co/KXWYK4S/asus.webp",
+    image: "https://i.ibb.co/KFX5k1R/asus.webp",
     min: 1,
     category: 'windows'
   },
@@ -23,7 +23,7 @@ export const films = [
     name: "Laptop Asus",
     price: 9000,
     stock: 10,
-    image: 'https://i.ibb.co/y6SpcxP/asus1.webp',
+    image: 'https://i.ibb.co/2ZJV5Gs/asus1.webp',
     category: 'windows'
   },
   {
@@ -31,7 +31,7 @@ export const films = [
     name: "Laptop Hp",
     price: 20000,
     stock: 7,
-    image: "https://i.ibb.co/P5V4Qnx/hp.webp",
+    image: "https://i.ibb.co/gm6mXZp/hp.webp",
     min: 1,
     category: 'windows'
   },
@@ -40,7 +40,7 @@ export const films = [
     name: "Laptop Leonovo",
     price: 17000,
     stock: 6,
-    image: "https://i.ibb.co/NCcZBxb/leonovo.webp",
+    image: "https://i.ibb.co/D7nk1ks/leonovo.webp",
     min: 1,
     category: 'windows'
   },
@@ -49,16 +49,16 @@ export const films = [
     name: "Laptop Logitech",
     price: 6500,
     stock: 11,
-    image: "https://i.ibb.co/yhwmp3n/logitech.webp",
+    image: "https://i.ibb.co/8d6stH1/logitech.webp",
     min: 1,
     category: 'windows'
   },
   {
     id: 7,
-    name: "Macbook Air",
+    name: "Macbook Air Intel",
     price: 25000,
     stock: 9,
-    image: "https://i.ibb.co/txrtH2m/macbook.webp",
+    image: "https://i.ibb.co/ZgqS50M/macbook.webp",
     min: 1,
     category: 'mac'
   },
@@ -67,7 +67,7 @@ export const films = [
     name: "Laptop Huawei",
     price: 9500,
     stock: 10,
-    image: "https://i.ibb.co/R4THHqG/matebook.webp",
+    image: "https://i.ibb.co/s3B2V9p/matebook.webp",
     min: 1,
     category: 'windows'
   },
@@ -76,9 +76,29 @@ export const films = [
     name: "Laptop Surface",
     price: 12000,
     stock: 2,
-    image: "https://i.ibb.co/3YsCNhF/surface.webp",
+    image: "https://i.ibb.co/DzGnXN1/surface.webp",
     min: 1,
     category: 'windows'
+  },
+  {
+    id: 10,
+    name: "Mac Air M2",
+    price:  29000,
+    stock: 10,
+    description:'CPU de 8 núcleos GPU de 8 núcleos Memoria unificada de 8 GB Almacenamiento SSD de 256 GB¹',
+    image: "https://i.ibb.co/4FRpPgp/macbook-air-silver-select-20220606.webp",
+    min: 1,
+    category: 'mac'
+  },
+  {
+    id: 11,
+    name: "Mac Mini M1",
+    price: 17999,
+    stock: 2,
+    description:'Chip M1 de Apple con CPU de 8 núcleos y GPU de 8 núcleos Almacenamiento de 256 GB',
+    image: "https://i.ibb.co/Ws9hn21/mac-mini-hero-202011.webp",
+    min: 1,
+    category: 'mac'
   },
 
 ];
