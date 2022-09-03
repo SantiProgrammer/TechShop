7710a12f66a4db01f4d24cac64ef99cd1f903eb7
santi-iztli <santi.iztli@gmail.com>
2022-09-03 15:30:06 -0500

    cart css

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,8 +1,9 @@
 const Cart = () =>{
     return(
         <>
-        <div className="cart">
+        <div className="cart-container">
             <h2>Cart</h2>
+            <div className="cart-card"></div>
 
         </div>
         </>
