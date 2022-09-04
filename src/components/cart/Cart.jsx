3a7af7bfc31ee3aa16d9fcf0fc4da5c0be36dce4
santi-iztli <santi.iztli@gmail.com>
2022-09-04 16:08:38 -0500

    upadte css shadows

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,7 +2,7 @@ const Cart = () =>{
     return(
         <>
         <div className="cart-container">
-            <h2>Cart</h2>
+            <h2>Checkout <img src="https://cdn-icons-png.flaticon.com/512/8354/8354645.png" alt="" /> </h2>
             <div className="cart-card"></div>
 
         </div>
--- src/components/cartWidget/CartWidget.jsx
+++ src/components/cartWidget/CartWidget.jsx
@@ -5,7 +5,7 @@ function CartWidget() {
     <>
     <div className="cart-widget">
       <a href="#app">
-        <img src='https://i.ibb.co/yY29KQz/carrito.png' className="carrito" alt="carrito"  />
+        <img src='https://cdn-icons-png.flaticon.com/512/3523/3523887.png' className="carrito" alt="carrito"  />
       </a>
     <p>5</p>
     </div>
