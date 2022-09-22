13c85f91d4002e88d301dc5059a993d835bce4f1
santi-iztli <santi.iztli@gmail.com>
2022-09-22 15:58:13 -0500

    Desafío: Item Collection II

--- src/components/cart/Index.jsx
+++ src/components/cart/Index.jsx
@@ -2,9 +2,51 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext'
 import ItemCart from '../ItemCart/Index';
+import { doc, collection, serverTimestamp, setDoc, updateDoc, increment } from 'firebase/firestore';
+import db  from '../../firebase/config';
+import swal from 'sweetalert';
 
 const Cart = () => {
     const { cart,totalPrice,clearCart} = useCartContext();
+
+
+    const createOrder = () => {
+        let order = {
+            buyer: {
+                name: 'Juan',
+                email: 'Juan123@gmail.com',
+                phone: '1234567890',
+                address: 'Calle falsa 123'
+
+            },
+            items: cart.map(producto => ({id: producto.id, title: producto.name, price: producto.price, quantity: producto.qty})),
+            total: totalPrice(),
+            date: serverTimestamp()
+        }
+        const createOrderInFirebase = async () => {
+            const newOrderRef = doc(collection(db, "orders"));
+            await setDoc(newOrderRef, order);
+            return newOrderRef;
+        }
+
+
+        createOrderInFirebase()
+        .then(result => swal('Orden creada con exito! Id: ' + result.id))
+        .catch(error => console.log(error))
+
+
+        cart.forEach(async(item) => {
+            const itemRef = doc(db, "products", item.id);
+            await updateDoc(itemRef, {
+                stock: increment(-item.qty)
+            });
+
+        })
+
+        clearCart();    
+    }
+
+
 
     if(cart.length === 0){
         return(
@@ -30,6 +72,7 @@ const Cart = () => {
             <div className='cart-footer' >
                 <p>Total: $ {totalPrice()}</p>
                 <button onClick={() => clearCart()} >Vaciar carrito</button>
+                <button onClick={createOrder} >Terminar la compra</button>
             </div>
             </div>
             </>
