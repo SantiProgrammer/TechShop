37a88aa9ec37a524dc1b52eb5f651bb18f34c89d
SantiagoMorera <santi.iztli@gmail.com>
2023-08-26 17:23:35 -0600

    traceIp

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,30 @@ function App() {
           </Routes>
         </CartProvider>
       </BrowserRouter>
+
+      <div
+        id="elemID031021"
+        style="line-height:16px;text-align:center;position:relative;z-index:100000;"
+      >
+        <script
+          async
+          type="text/javascript"
+          src="//s2.tracemyip.org/vLg/lgUrl.php?pidnVar2=100993&amp;prtVar2=11&amp;stlVar2=1500~1693092042~14*2~0F5999*F7FFE6*537899*000000~1*1*0*0*0&amp;rgtype=4684NR-IPIB&amp;scvVar2=12"
+        ></script>
+        <noscript>
+          <a
+            title="cookieless user user tracker"
+            href="https://www.tracemyip.org/tools/cookie-free-tracking-analytics-eu-cookie-law-solution/"
+          >
+            <img
+              src="//s2.tracemyip.org/vLg/1500~1693092042~14*2~0F5999*F7FFE6*537899*000000~1*1*0*0*0/4684NR-IPIB/100993/11/12/ans/"
+              alt="Cookieless user user tracker"
+              referrerpolicy="no-referrer-when-downgrade"
+              style="border:0px;"
+            />
+          </a>
+        </noscript>
+      </div>
     </div>
   );
 }
